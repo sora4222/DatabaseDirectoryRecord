b012a7f7bdd574211df5784cbe3c83ea58ead00e
Jesse Ross <sora4222@gmail.com>
2019-11-23 16:55:10 +1100

    Creates a mysql setup script.

---
+++ scripts/mySqlCreateTableStatement.sql
@@ -0,0 +1,16 @@
+# CREATE TABLE directory_records(
+#     FilePath varchar(2500),
+#     FileHash varchar(100),
+#     ComputerName varchar(300),
+#     DatabaseRowCreationTime datetime default CURRENT_TIMESTAMP,
+#     PRIMARY KEY(FilePath, ComputerName)
+# );
+# The script above is preferable but will result in: Specified key was too long; max key length is 3072 bytes
+CREATE TABLE directory_records
+(
+    FilePath                varchar(600),
+    FileHash                varchar(100),
+    ComputerName            varchar(100),
+    DatabaseRowCreationTime datetime default CURRENT_TIMESTAMP,
+    PRIMARY KEY (FilePath, ComputerName)
+);
